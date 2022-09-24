823d97f44a532fefe4424272bff70df17bb7564d
Cristian Sanchez <mariocrsanchez@gmail.com>
2022-09-24 20:38:39 -0300

    creamos funcion escogerPalabraSecreta

--- ahorcado.js
+++ ahorcado.js
@@ -3,6 +3,12 @@ let tablero = document.getElementById("forca").getContext("2d");
 let palabraSecreta = "";
 
 
+function escogerPalabraSecreta(){
+    let palabra = palabras[Math.floor(Math.random() * palabras.length)]
+    palabraSecreta = palabra;
+    console.log(palabraSecreta)
+}
+
 
 function iniciarJuego(){
     document.getElementById("iniciar-juego").style.display = "none";
