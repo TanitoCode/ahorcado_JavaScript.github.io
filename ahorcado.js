2dd49292fdd9c2fa5c09682f79c7800471d1b624
Cristian Sanchez <mariocrsanchez@gmail.com>
2022-09-24 19:56:40 -0300

    Creado archivo ahorcado.js

---
+++ ahorcado.js
@@ -0,0 +1,9 @@
+let palabras = ["CRISTIAN", "LAUTARO", "SIMON","ISABEL"];
+let tablero = document.getElementById("forca").getContext("2d");
+let palabraSecreta = "";
+
+
+
+function iniciarJuego(){
+    document.getElementById("iniciar-juego").style.display = "none";
+}
